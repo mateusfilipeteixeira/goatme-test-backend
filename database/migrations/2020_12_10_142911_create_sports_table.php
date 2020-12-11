5acbb581a07d51a68508e7ae5a4876791825b8ec
Mateus Teixeira <mateusfilipeteixeira@gmail.com>
2020-12-11 02:30:07 -0300

    :tada: Initial Commit

---
+++ database/migrations/2020_12_10_142911_create_sports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sports', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->date('schedule_date');
+            $table->time('start');
+            $table->time('end');
+            $table->boolean('recurrent')->default(false);
+            $table->integer('weeks')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sports');
+    }
+}
---
+++ database/migrations/2020_12_11_050456_create_schedules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sport');
+            $table->date('date')->nullable(false);
+            $table->time('start')->nullable(false);
+            $table->time('end')->nullable(false);
+            $table->timestamps();
+            $table->foreign('sport')
+                ->references('id')->on('sports')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
